b1eb81fcd0752ac3061370529d85f03c33ef4125
Wilgnne <w.khawanbarbosa@gmail.com>
2019-11-13 21:04:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo "Install Soccerpy"
 path="$(python3.7 -m site --user-site)"
 sudo rm -r $path/soccerpy;
 
-sudo git clone https://github.com/Wilgnne/soccerpy.git $path/soccerpy
+git clone https://github.com/Wilgnne/soccerpy.git $path/soccerpy
